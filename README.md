2524a700e188f41a1d6b8a3b9107fc6d4cdbbc77
최승환 <tazz0009@cj.net>
2022-02-07 16:44:32 +0900

    chap02 - add proof of work

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 go mod init github.com/tazz0009/go-blockchain
 ```
 
-2. badgerDB
+1. badgerDB
 
 ```cmd
 go get github.com/dgraph-io/badger/v3
@@ -16,4 +16,4 @@ go get github.com/dgraph-io/badger/v3
 
 ## Links
 
-참고 - (https://github.com/tensor-programming/golang-blockchain/blob/part_4/blockchain/blockchain.go)
+참고 - (<https://github.com/tensor-programming/golang-blockchain/blob/part_4/blockchain/blockchain.go>)
--- main_test.go
+++ main_test.go
@@ -1,45 +1,27 @@
 package main_test
 
 import (
-	"encoding/hex"
 	"fmt"
+	"math/big"
 	"testing"
-
-	main "github.com/tazz0009/go-blockchain"
 )
 
 type testCase struct {
-	inputData1     string
-	hashInputData1 string
-	inputData2     string
+	difficulty int
+	value      string
 }
 
-func TestMain_CreateBlock(t *testing.T) {
+func TestMain_target(t *testing.T) {
+	tc := testCase{16, "1000000000000000000000000000000000000000000000000000000000000"}
+	target := big.NewInt(1)
+	Difficulty := tc.difficulty
+	target.Lsh(target, uint(256-Difficulty))
 
-	tc := testCase{"Genesis", "81ddc8d248b2dccdd3fdd5e84f0cad62b08f2d10b57f9a831c13451e5c5c80a5", "First Block after Genesis"}
-	block := main.CreateBlock(tc.inputData1, []byte{})
+	fmt.Printf("target : %x\n", target)
 
-	fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-	fmt.Printf("Data in Block: %x\n", block.Data)
-	fmt.Printf("Hash: %x\n", block.Hash)
-
-	if tc.hashInputData1 != fmt.Sprintf("%x", block.Hash) {
-		t.Error("Wrong result")
+	if fmt.Sprintf("%x", target) != tc.value {
+		t.Error("Error!!!")
 	}
-
-	decodedByteArray, err := hex.DecodeString(tc.hashInputData1)
-
-	if err != nil {
-		fmt.Println("Unable to convert hex to byte. ", err)
-	}
-
-	fmt.Println(block.Hash)
-	fmt.Println(decodedByteArray)
-
-	encodedString := hex.EncodeToString(block.Hash)
-
-	fmt.Println(tc.hashInputData1)
-	fmt.Println(encodedString)
 }
 
 // func TestMain_DeriveHash(t *testing.T) {
